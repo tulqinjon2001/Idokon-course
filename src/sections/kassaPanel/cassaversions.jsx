444a3cce2a7f0020a777a53f6c9ac89481dc9fcc
tulqinjon2001 <107257306+tulqinjon2001@users.noreply.github.com>
2026-04-27 15:43:44 +0500

    Add new version entry for 1.3.5 in cassaversions

--- src/sections/kassaPanel/cassaversions.jsx
+++ src/sections/kassaPanel/cassaversions.jsx
@@ -26,6 +26,7 @@ function CassaVersions() {
     {id: 3,
      version: "1.3.5",
      url: "https://github.com/tulqinjon2001/Idokon-course/releases/download/Kassa/Idokon1.3.5.x64.exe",
+    }
   ];
 
   return (
