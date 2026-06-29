e6ee5ec5d25744256aaf3d6304f5911d3bb4a2fd
tulqinjon2001 <107257306+tulqinjon2001@users.noreply.github.com>
2026-06-29 14:27:19 +0500

    Add new version 1.3.8 to KassaVersiyalarPage

--- src/features/kassa/KassaVersiyalarPage.jsx
+++ src/features/kassa/KassaVersiyalarPage.jsx
@@ -33,6 +33,11 @@ export default function KassaVersiyalarPage() {
       version: "1.3.7",
       url: "https://github.com/tulqinjon2001/Idokon-course/releases/download/Kassa/Idokon1.3.7.x64.exe",
     },
+    {
+      id: 4,
+      version: "1.3.7",
+      url: "https://github.com/tulqinjon2001/Idokon-course/releases/download/Kassa/Idokon1.3.8.x64.exe",
+    },
   ];
 
   return (
